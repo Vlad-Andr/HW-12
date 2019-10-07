4a194c013051f583c33e016a318c9dd8dfd18493
Vlad-Andr <andreev.v99@gmail.com>
2019-10-07 12:08:58 +0300

    Fixed all remarks

--- src/main/java/edu/cursor/controller/UserController.java
+++ src/main/java/edu/cursor/controller/UserController.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("user")
 public class UserController {
 
     private final UserServiceImpl userServiceImpl = new UserServiceImpl();
 
-    @GetMapping("/user/{email}")
+    @GetMapping("/{email}")
     public User getUserByEmail(@PathVariable("email") String email) {
         return userServiceImpl.getInfoByUser(email);
     }
 
-    @PostMapping("addNewUser")
+    @PostMapping("/add")
     public void addUserToFile(@RequestBody User user) throws IOException {
         userServiceImpl.saveUserToFile(user);
     }
--- src/main/java/edu/cursor/userData/User.java
+++ src/main/java/edu/cursor/userData/User.java
@@ -29,35 +29,3 @@ public class User {
     @JsonIgnore
     private int accessId;
 }
-/*
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@JsonPropertyOrder({"accessId","name","surname","lastLoginDate","email","statusHW"})
-public class User {
-    @JsonIgnore
-    private int accessId;
-    private String name;
-    private String surname;
-    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd")
-    @JsonDeserialize(using = LocalDateDeserializer.class)
-    private LocalDate lastLoginDate;
-    private String email;
-    private Map<String,Boolean> statusHW;
-
-    public User(String name, String surname, String email) {
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-
-    }
-
-    public void setStatusHW(String hw1, String b1) {
-    }
-    public void setStatusHW(String hw1, boolean b1, String hw2, boolean b2) {
-    }
-    public void setStatusHW(String hw1, boolean b1, String hw2, boolean b2, String hw3, boolean b3) {
-    }
-    public void setStatusHW(String hw1, boolean b, String hw2, boolean b1, String hw3, boolean b3, String hw4, boolean b4) {
-    }
-}*/
